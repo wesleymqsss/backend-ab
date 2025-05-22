80c8df06b90068ec4199fbf4a0667c82565cec57
Wesley Marques <wesleymqsss@gmail.com>
2025-05-22 13:24:50 -0300

    feature/login - criacao da controller de login

--- apiajudabrasil/src/main/java/io/github/ajudabrasil/apiajudabrasil/controller/UsuarioController.java
+++ apiajudabrasil/src/main/java/io/github/ajudabrasil/apiajudabrasil/controller/UsuarioController.java
@@ -2,13 +2,15 @@ package io.github.ajudabrasil.apiajudabrasil.controller;
 
 import io.github.ajudabrasil.apiajudabrasil.model.Usuario;
 import io.github.ajudabrasil.apiajudabrasil.repository.UsuarioRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("teste")
+@RequestMapping("usuario")
 public class UsuarioController {
 
     private final UsuarioRepository usuarioRepository;
@@ -33,11 +35,22 @@ public class UsuarioController {
 
     @GetMapping("{id}")
     public Usuario obterPorId(@PathVariable("id") String id){
-        return usuarioRepository.findById(id).orElse(null);
+        return usuarioRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado com o ID: " + id));
+    }
+
+    @GetMapping("/por-tipo-perfil/{tipo}")
+    public List<Usuario> getUsuarioPorPerfil(@PathVariable("tipo") Integer tipoPerfil){
+        return usuarioRepository.findByTipoPerfil(tipoPerfil);
     }
 
     @DeleteMapping("{id}")
     public void deletar (@PathVariable("id") String id){
         usuarioRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Usuario usuario){
+        usuario.setId(id);
+        usuarioRepository.save(usuario);
     }
 }
--- apiajudabrasil/src/main/java/io/github/ajudabrasil/apiajudabrasil/repository/UsuarioRepository.java
+++ apiajudabrasil/src/main/java/io/github/ajudabrasil/apiajudabrasil/repository/UsuarioRepository.java
@@ -3,6 +3,8 @@ package io.github.ajudabrasil.apiajudabrasil.repository;
 import io.github.ajudabrasil.apiajudabrasil.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+import java.util.List;
 
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    List<Usuario> findByTipoPerfil(Integer tipoPerfil);
 }
